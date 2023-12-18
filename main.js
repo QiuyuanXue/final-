5172126d1ad333a069f0de599465c41c80a02a4d
QiuyuanXue <xueqiuyuan1110@gmail.com>
2023-12-18 09:30:26 -0500

    。

---
+++ main.js
@@ -0,0 +1,6 @@
+let level1 = document.querySelector('.item--2')
+let level2 = document.querySelector('.item--3')
+let level3 = document.querySelector('.item--4')
+let level4 = document.querySelector('.item--5')
+
+
